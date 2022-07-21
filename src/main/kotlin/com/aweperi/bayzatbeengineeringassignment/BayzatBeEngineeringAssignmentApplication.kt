c876c303b4e6f22ab6e66d9233c34c5c55fe60ff
Emmanuel Adiba <eabaagah@gmail.com>
2022-07-21 11:13:27 +0000

    setup project

---
+++ src/main/kotlin/com/aweperi/bayzatbeengineeringassignment/BayzatBeEngineeringAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.aweperi.bayzatbeengineeringassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BayzatBeEngineeringAssignmentApplication
+
+fun main(args: Array<String>) {
+    runApplication<BayzatBeEngineeringAssignmentApplication>(*args)
+}
